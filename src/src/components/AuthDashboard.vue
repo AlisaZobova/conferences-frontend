015d688ac999bc1946c0482833fd9375b8d4a18a
Alisa Zobova <zobovaalice@gmail.com>
2023-01-17 20:37:13 +0200

    feat: add login component

---
+++ src/src/components/AuthDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-list-item-subtitle v-on:click="logout">log out</v-list-item-subtitle>
+  </v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$router.replace({name: 'login'})
+    }
+  }
+}
+</script>
