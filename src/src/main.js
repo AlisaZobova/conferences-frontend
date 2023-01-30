aead7ca27f38fa7b4716504a31ad846ef16f259b
Alisa Zobova <zobovaalice@gmail.com>
2023-01-30 17:35:40 +0200

    fix: add user logout when get 403 error from server

--- src/src/main.js
+++ src/src/main.js
@@ -27,7 +27,7 @@ axios.interceptors.response.use(function (response) {
     return response
 }, async function (error) {
     if (error.response) {
-        if (error.response.status === 401) {
+        if (error.response.status === 401 || error.response.status === 403) {
             setCSRFToken();
             store.state.auth.user = null;
             return router.push('/login');
