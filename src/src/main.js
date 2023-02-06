4c48a4cfafb75cae2a59c63affa473f9b443327a
Alisa Zobova <zobovaalice@gmail.com>
2023-02-06 15:04:17 +0200

    fix: move axios settings to ApiClient.js file

--- src/src/main.js
+++ src/src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import './js/ApiClient'
 
 import VueTelInput from 'vue-tel-input'
 import 'vue-tel-input/dist/vue-tel-input.css';
@@ -26,48 +25,6 @@ Vue.use(VueGoogleMaps, {
         libraries: 'places',
     },
 })
-
-axios.defaults.withCredentials = true
-axios.defaults.baseURL = process.env.VUE_APP_AXIOS_BASE_URL;
-axios.defaults.headers.common['Accept'] = `application/json`;
-
-axios.interceptors.response.use(function (response) {
-    return response
-}, async function (error) {
-    if (error.response) {
-        if (error.response.status === 401 || error.response.status === 403) {
-            setCSRFToken();
-            store.state.auth.user = null;
-            return router.push('/login');
-        }
-    }
-    if (error.response.status === 404) {
-        return router.push('/404').catch(() => {
-        });
-    }
-    return Promise.reject(error)
-})
-
-
-const onRequest = (config) => {
-    if ((
-            config.method === 'post' ||
-            config.method === 'put' ||
-            config.method === 'delete'
-        ) &&
-        !Cookies.get('XSRF-TOKEN')) {
-        return setCSRFToken().then(() => {
-            return config
-        });
-    }
-    return config;
-}
-
-const setCSRFToken = () => {
-    return axios.get('/sanctum/csrf-cookie');
-}
-
-axios.interceptors.request.use(onRequest, null);
 
 Vue.config.productionTip = false
 
