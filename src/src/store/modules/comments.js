2379b75c2204d80eab9ae67462e5ba30eca7f59d
Alisa Zobova <zobovaalice@gmail.com>
2023-02-06 11:39:14 +0200

    fix: delete unusable comment variable in comments store state

--- src/src/store/modules/comments.js
+++ src/src/store/modules/comments.js
@@ -2,34 +2,27 @@ import axios from 'axios';
 
 const state = {
     comments: [],
-    comment: null,
 };
 
 const actions = {
     async GetComments({commit}, reportId) {
         let response = await axios.get('reports/' + reportId + '/comments')
         await commit("setComments", response.data)
-        await commit("setComment", null)
     },
-    async UpdateComment({commit}, {form, commentId}) {
-        let response = await axios.patch('comments/' + commentId, form)
-        await commit("setComment", response.data)
+    async UpdateComment(context, {form, commentId}) {
+        await axios.patch('comments/' + commentId, form)
     },
-    async CreateComment({commit}, form) {
-        let response = await axios.post('comments', form, {
+    async CreateComment(context, form) {
+        await axios.post('comments', form, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
         })
-        await commit("setComment", response.data)
     },
 };
 const mutations = {
     setComments(state, comments) {
         state.comments = comments
-    },
-    setComment(state, comment) {
-        state.comment = comment
     },
 };
 
