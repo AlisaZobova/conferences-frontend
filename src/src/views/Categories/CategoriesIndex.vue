e76f3afb07e040081c0081036a3aa9ab03792795
Alisa Zobova <zobovaalice@gmail.com>
2023-02-15 16:49:49 +0200

    fix: fix v-treeview component style

--- src/src/views/Categories/CategoriesIndex.vue
+++ src/src/views/Categories/CategoriesIndex.vue
@@ -1,61 +1,66 @@
 <template>
-  <v-main>
-    <div v-if="loading" class="text-center">
-      <v-progress-circular
-          indeterminate
-          color="primary"
-      ></v-progress-circular>
-    </div>
-    <div v-else>
-      <v-layout class="justify-start align-start">
-        <v-btn :to="{ name: 'CreateCategory' }" color="primary">Create Category</v-btn>
-      </v-layout>
-      <v-layout class="justify-center align-center mt-4 blue lighten-5 rounded">
-        <v-treeview
-            :items="categories"
-            activatable
-            rounded
-            hoverable
-        >
-          <template #label="{ item }">
-            <router-link class="d-block text-decoration-none" :to="{ name: 'ShowCategory', params: { id: item.id } }">
-              {{ item.name }}
-            </router-link>
-          </template>
-        </v-treeview>
-      </v-layout>
-    </div>
-  </v-main>
+    <v-main>
+        <div v-if="loading" class="text-center">
+            <v-progress-circular
+                indeterminate
+                color="primary"
+            ></v-progress-circular>
+        </div>
+        <div v-else>
+            <v-layout class="justify-start align-start">
+                <v-btn :to="{ name: 'CreateCategory' }" color="primary"
+                    >Create Category</v-btn
+                >
+            </v-layout>
+            <v-layout
+                class="justify-center align-center mt-4 blue lighten-5 rounded"
+            >
+                <v-treeview :items="categories" activatable hoverable>
+                    <template #label="{ item }">
+                        <router-link
+                            class="d-block text-decoration-none"
+                            :to="{
+                                name: 'ShowCategory',
+                                params: { id: item.id },
+                            }"
+                        >
+                            {{ item.name }}
+                        </router-link>
+                    </template>
+                </v-treeview>
+            </v-layout>
+        </div>
+    </v-main>
 </template>
 
 <script>
-import {mapActions} from "vuex";
+import { mapActions } from 'vuex'
 
 export default {
-  name: "CategoriesIndex",
-  computed: {
-    categories() {
-      return this.$store.state.categories.categories.filter(
-          (category) => !category.parents
-      )
-    }
-  },
-  data() {
-    return {
-      loading: true
-    }
-  },
-  methods: {
-    ...mapActions(['GetCategories']),
-  },
-  created() {
-    this.GetCategories().then(() => this.loading = false)
-  }
+    name: 'CategoriesIndex',
+    computed: {
+        categories() {
+            return this.$store.state.categories.categories.filter(
+                (category) => !category.parents
+            )
+        },
+    },
+    data() {
+        return {
+            loading: true,
+        }
+    },
+    methods: {
+        ...mapActions(['GetCategories']),
+    },
+    created() {
+        this.GetCategories().then(() => (this.loading = false))
+    },
 }
 </script>
 
 <style scoped>
 :deep(.v-treeview) {
-  width: 95%;
+    width: 100%;
 }
 </style>
