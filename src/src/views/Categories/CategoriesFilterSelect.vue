74c4c1dcd99aa3404e3f626145b7daeb1f419df5
Alisa Zobova <zobovaalice@gmail.com>
2023-02-16 19:32:37 +0200

    feat: create component with multiple tree combobox select for categories

---
+++ src/src/views/Categories/CategoriesFilterSelect.vue
@@ -0,0 +1,83 @@
+<template>
+    <v-combobox
+        :loading="loading"
+        :items="items"
+        item-text="name"
+        v-model="selected"
+        @change="toggle"
+        label="Choose categories"
+        multiple
+    >
+        <template v-slot:append-item>
+            <v-divider class="mt-2"></v-divider>
+            <v-treeview
+                :items="rootCategories"
+                v-model="category"
+                @input="updateSelected"
+                selectable
+                selection-type="independent"
+            >
+            </v-treeview>
+        </template>
+    </v-combobox>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+    name: 'CategoriesFilterSelect',
+    data() {
+        return {
+            loading: true,
+            category: [],
+            items: [{ name: 'All categories' }],
+            selected: [],
+        }
+    },
+    computed: {
+        categories() {
+            return this.$store.state.categories.categories
+        },
+        rootCategories() {
+            return this.categories.filter((category) => !category.parents)
+        },
+        categoriesIds() {
+            return this.categories.map((category) => category.id)
+        },
+    },
+    methods: {
+        ...mapActions(['GetCategories']),
+        toggle() {
+            if (this.category.length === this.categoriesIds.length) {
+                this.category = []
+                this.selected = []
+            } else {
+                this.category = this.categoriesIds
+                this.selected = this.categories
+            }
+        },
+        updateSelected(value) {
+            this.selected = this.categories.filter((category) =>
+                value.includes(category.id)
+            )
+        },
+    },
+    created() {
+        this.GetCategories().then(() => (this.loading = false))
+    },
+}
+</script>
+
+<style scoped>
+:deep(.v-list-item__action) {
+    display: none;
+}
+
+:deep(.v-list-item--link:before) {
+    background-color: white;
+}
+:deep(.v-list-item--link:hover) {
+    background-color: #eeeeee;
+}
+</style>
