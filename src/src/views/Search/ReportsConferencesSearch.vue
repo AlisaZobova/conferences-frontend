539c4a0f1793b628afdfd48d87e3d2d5ddf23bac
Alisa Zobova <zobovaalice@gmail.com>
2023-03-01 18:25:30 +0200

    fix: fix search menu

--- src/src/views/Search/ReportsConferencesSearch.vue
+++ src/src/views/Search/ReportsConferencesSearch.vue
@@ -4,7 +4,6 @@
             offset-y
             :close-on-content-click="false"
             v-model="menu"
-            :left="false"
             min-width="300"
             transition="slide-y-transition"
         >
